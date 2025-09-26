f3655838f1621b14f3ceb1a2b332425f33fc4eb3
inucreativehrd21 <moon95567@inu.ac.kr>
2025-09-26 16:21:20 +0900

    refactoring code

--- config/settings/base.py
+++ config/settings/base.py
@@ -12,9 +12,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# .env 파일 로드
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
