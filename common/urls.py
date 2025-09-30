a80c595aa6b7bd20a5ee06061f7219c872b2d3ca
inucreativehrd21 <moon95567@inu.ac.kr>
2025-09-30 20:31:06 +0900

    email settings

--- common/urls.py
+++ common/urls.py
@@ -8,7 +8,7 @@ app_name = 'common'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('signup/', views.signup_with_email_verification, name='signup'),
+    path('signup/', views.signup, name='signup'),
     path('theme/', views.save_theme, name='save_theme'),
     path('profile/edit/', views.profile_edit, name='profile_edit'),
     path('password/change/', auth_views.PasswordChangeView.as_view(template_name='common/password_change.html', success_url='/common/password/change/done/'), name='password_change'),
